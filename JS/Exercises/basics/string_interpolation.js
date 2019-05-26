96ec64bdf2c2d2376dfee2ad702d63daed9040de
Arka Roy <arka243roy@googlemail.com>
2019-05-26 21:43:49 +0100

    Add files via upload

---
+++ JS/Exercises/basics/string_interpolation.js
@@ -0,0 +1,3 @@
+let myName = 'Arka Roy';
+let myCity = 'Kolkata';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
---
+++ JS/Exercises/basics/variable_string_concatenation.js
@@ -0,0 +1,2 @@
+let favoriteAnimal = 'Tiger';
+console.log('My favorite animal: ' + favoriteAnimal);
