b081b42f98ca58f64703cfb00e43f9ca24980807
Arka Roy <arka243roy@googlemail.com>
2019-06-04 08:30:22 +0100

    Add files via upload

---
+++ JS/Exercises/REACT/stateless/stateless/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Parent from "./components/Parent/Parent"
+
+class App extends React.Component{
+  render(){
+    return <Parent />;
+  }
+}
+
+export default App;
