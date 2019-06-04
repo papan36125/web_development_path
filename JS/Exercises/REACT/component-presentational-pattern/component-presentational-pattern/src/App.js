5b1e2a9993ccef5f364fd2866e4a02176b0fcf3a
Arka Roy <arka243roy@googlemail.com>
2019-06-04 08:32:18 +0100

    Add files via upload

---
+++ JS/Exercises/REACT/component-presentational-pattern/component-presentational-pattern/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {GuineaPigsContainer} from './containers/GuineaPigsContainer'
+
+class App extends React.Component{
+  render(){
+    return <GuineaPigsContainer />;
+  }
+}
+
+export default App;
